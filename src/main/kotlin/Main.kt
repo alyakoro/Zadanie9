31af2b8176fc9443897e0e1437ed106bb95e0da6
1101-22 <alya.korobeynikova99@gmail.com>
2023-11-18 15:33:00 +0300

    z9

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,42 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlinx.coroutines.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+suspend fun main() = coroutineScope {
+    print("Введите через сколько секунд сработает 1 будильник 'tick': ")
+    val b_1 = readln().toInt()
+    print("Введите через сколько секунд сработает 1 будильник 'click': ")
+    val b_2 = readln().toInt()
+    print("Введите через сколько секунд сработает 1 будильник 'hrack': ")
+    val b_3 = readln().toInt()
+    launch {
+        for (i in 0.. (maxOf(b_1,b_2,b_3))){
+            delay(1000L)
+            println()
+        }
+    }
+    launch {
+        for (i in 1.. b_1){
+            delay(1000L)
+            print("tick\t")
+        }
+        delay(1000L)
+        println("=\tBoom")
+    }
+    launch {
+        for (i in 1.. b_2){
+            delay(1000L)
+            print("click\t")
+        }
+        delay(1000L)
+        println("=\tBoom")
+    }
+    launch {
+        for (i in 1.. b_3){
+            delay(1000L)
+            print("hrack\t")
+        }
+        delay(1000L)
+        println("=\tBoom")
+    }
+
+    println("Будильники начали работу")
 }
